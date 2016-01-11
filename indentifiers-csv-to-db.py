2f1f8b0c24a6d56182eaf6b265b4aa6b1f3bf04c
Alessio Bogon <youtux@gmail.com>
2016-01-11 15:49:02 +0100

    identifiers-csv-to-db: each entity is an appearance of an identifier in a page

--- indentifiers-csv-to-db.py
+++ indentifiers-csv-to-db.py
@@ -4,14 +4,26 @@ import pathlib
 import sqlite3
 import functools
 import dateutil.parser
+import itertools
+import collections
 
 from utils import *
 
 insert_tpl = '''
-INSERT INTO identifiers_history VALUES (?, ?, ?, ?, ?, ?, ?)
+INSERT INTO identifiers_history VALUES (?, ?, ?, ?, ?, ?)
 '''
 
 timestamp_parser_cached = functools.lru_cache(100000)(dateutil.parser.parse)
+
+InputRecord = collections.namedtuple(
+    'InputRecord',
+    'identifier_type identifier_id action timestamp project page_id revision_id',
+)
+
+OutputRecord = collections.namedtuple(
+    'OutputRecord',
+    "identifier_type identifier_id start_date end_date project page_id",
+)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -19,6 +31,7 @@ def parse_args():
         'input_files',
         nargs='+',
         type=pathlib.Path,
+        help='''Input csv files. They must be ordered by ([project], page, timestamp)''',
     )
     parser.add_argument(
         'sqlite_file',
@@ -39,16 +52,15 @@ def create_tables_and_indexes(connection):
 CREATE TABLE IF NOT EXISTS identifiers_history (
     identifier_type TEXT NOT NULL,
     identifier_id TEXT NOT NULL,
-    "action" INTEGER NOT NULL,
-    timestamp DATETIME NOT NULL,
+    start_date DATETIME,
+    end_date DATETIME,
     project TEXT NOT NULL,
     page_id INTEGER NOT NULL,
-    revision_id INTEGER NOT NULL,
-    PRIMARY KEY (identifier_type, identifier_id, "action", timestamp, project, page_id, revision_id)
+    PRIMARY KEY (identifier_type, identifier_id, start_date, end_date, project, page_id)
 );
 
 -- Index: timestamp_asc
-CREATE INDEX IF NOT EXISTS timestamp_asc ON identifiers_history (timestamp ASC);
+-- CREATE INDEX IF NOT EXISTS timestamp_asc ON identifiers_history (timestamp ASC);
 
 -- Index: identifier_asc
 CREATE INDEX IF NOT EXISTS identifier_asc ON identifiers_history (
@@ -59,10 +71,21 @@ CREATE INDEX IF NOT EXISTS identifier_asc ON identifiers_history (
 -- Index: page_revision_asc
 CREATE INDEX IF NOT EXISTS project_page_revision_asc ON identifiers_history (
     project ASC,
-    page_id ASC,
-    revision_id ASC
+    page_id ASC
 );
     ''')
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 def parse_record(r, default_project=None):
     # no project given
@@ -73,7 +96,27 @@ def parse_record(r, default_project=None):
         identifier_type, identifier_id, action, timestamp, project, page_id, revision_id = r
 
     timestamp = timestamp_parser_cached(timestamp)
-    return identifier_type, identifier_id, action, timestamp, project, page_id, revision_id
+    return InputRecord(identifier_type, identifier_id, action, timestamp, project, page_id, revision_id)
+
+def merge_records(records):
+    for _, group in itertools.groupby(records, key=lambda r: (r.project, r.page_id)):
+        key_by_project_page_id_ts = lambda r: (r.project, r.page_id, r.identifier_type, r.identifier_id, r.timestamp)
+        group_per_page = sorted(group, key=key_by_project_page_id_ts)
+
+        key_by_project_page_id = lambda r: (r.project, r.page_id, r.identifier_type, r.identifier_id)
+        for key, group in itertools.groupby(group_per_page,  key=key_by_project_page_id):
+            for r1, r2 in grouper(group, 2, fillvalue=InputRecord(None, None, 'removed', None, None, None, None)):
+                assert r1.action != r2.action
+                assert r1.revision_id != r2.revision_id
+
+                yield OutputRecord(
+                    r1.identifier_type,
+                    r1.identifier_id,
+                    r1.timestamp,
+                    r2.timestamp,
+                    r1.project,
+                    r1.page_id,
+                )
 
 def main():
     args = parse_args()
@@ -89,9 +132,12 @@ def main():
         input_file = open_compressed_file(file_path)
         with input_file, conn:
             csvreader = csv.reader(input_file)
-            records = (parse_record(r, 'en') for r in csvreader)
+            input_records = (parse_record(r, 'en') for r in csvreader)
 
-            conn.executemany(insert_tpl, records)
+            # for r in merge_records(input_records):
+            #     print(r)
+
+            conn.executemany(insert_tpl, merge_records(input_records))
 
 
 if __name__ == '__main__':
